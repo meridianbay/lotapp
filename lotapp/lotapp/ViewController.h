cd63e5a5222a6242e8ffc9e2fb8788997918b7e3
Junjie <jwang@serbian-spruce.local>
2015-03-15 16:25:12 -0700

    hello world

---
+++ lotapp/lotapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lotapp
+//
+//  Created by Junjie Wang on 3/15/15.
+//  Copyright (c) 2015 MeridianBay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
